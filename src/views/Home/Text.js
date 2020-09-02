017008d04ceb357a5e471d4837612f454aed0cb7
Julie <julie.salvo1@gmail.com>
2020-09-03 01:27:05 +0200

    fix responsive

--- src/views/Home/Text.js
+++ src/views/Home/Text.js
@@ -7,7 +7,7 @@ const TextHome =() =>{
     <div className="text-home">
         <div>
             <h1 className="animate__animated animate__backInLeft">Julie Ramis</h1>
-            <h2 className="animate__animated animate__backInRight text-dev-home">Développeuse Front-End</h2>
+            <h2 className="animate__animated animate__backInLeft text-dev-home">Développeuse Front-End</h2>
             <a href="#link-presentation" className="button">En savoir plus</a>
         </div>
     </div>
